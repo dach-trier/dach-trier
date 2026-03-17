0be7e240b4bd0118ef16a93f28aafa286692d004
Volodymyr But <git@hiegz.com>
2026-03-17 15:27:45 +0100

    adjust web server to serve index page and its assets

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -12,6 +12,17 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(chi_middleware.Logger)
 
+	fs := http.FileServer(http.Dir("./static"))
+	r.Handle("/static/*", http.StripPrefix("/static/", fs))
+
+	r.Get("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./favicon.ico")
+	})
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./templates/index.html")
+	})
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("404 not found"))
 	})
