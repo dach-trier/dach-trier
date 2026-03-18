a2cb13fe3924abfe2c71454fbed065bc447ea3ec
Volodymyr But <git@hiegz.com>
2026-03-18 23:44:41 +0100

    implement image archive carousel

--- client/index.ts
+++ client/index.ts
@@ -1,9 +1,13 @@
 import { registerHoverCapabilityController } from "@client/hover";
+import { ArchiveCarousel } from "./archive-carousel";
 import { me } from "@client/inline";
 
 if (window !== undefined) {
     // inline.ts exports
     (window as any).me = me;
+
+    // archive-carousel.ts exports
+    (window as any).ArchiveCarousel = ArchiveCarousel;
 }
 
 registerHoverCapabilityController();
