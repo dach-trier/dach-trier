65a2097d2057cf787df903bb10cc6e991c2de361
Volodymyr But <git@hiegz.com>
2026-03-26 00:30:10 +0100

    rewrite index template using tailwind and inline scripts

--- web/app.go
+++ web/app.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 
+	"html/template"
+
 	chi "github.com/go-chi/chi/v5"
 	chi_middleware "github.com/go-chi/chi/v5/middleware"
 )
@@ -19,12 +21,16 @@ func (app *App) Router() http.Handler {
 	router := chi.NewRouter()
 	router.Use(chi_middleware.Logger)
 
+	// images
+
+	fs := http.FileServer(http.Dir("web/assets/images"))
+	router.Get("/assets/images/*", http.StripPrefix("/assets/images/", fs).ServeHTTP)
+
 	// assets
 
 	router.Get("/favicon.ico", app.serveTabIcon)
 	router.Get("/assets/graphics/dach-flat.svg", app.serveFlatLogo)
 	router.Get("/assets/graphics/dach.svg", app.serveDetailedLogo)
-	router.Get("/assets/graphics/icon-sprite.svg", app.serveIconSprite)
 	router.Get("/assets/styles/bundle.css", app.serveStyleBundle)
 
 	// pages
@@ -47,16 +53,18 @@ func (app *App) serveFlatLogo(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "web/assets/graphics/dach-flat.svg")
 }
 
-func (app *App) serveIconSprite(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "web/assets/graphics/sprite.svg")
-}
-
 func (app *App) serveStyleBundle(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "web/assets/styles/bundle.css")
 }
 
 func (app *App) serveIndexPage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./templates/index.html")
+	var tmpl *template.Template
+
+	tmpl = template.New("")
+	tmpl = template.Must(tmpl.ParseFiles("web/templates/index.html"))
+	tmpl = template.Must(tmpl.ParseGlob("web/icons/*.svg"))
+
+	tmpl.ExecuteTemplate(w, "page", map[string]any{})
 }
 
 func (app *App) serveNotFoundPage(w http.ResponseWriter, r *http.Request) {
